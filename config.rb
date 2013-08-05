160c56d9896522a6fee164b6bfd1b79c470ad31f
Patrick O'Doherty <p@trickod.com>
2013-08-04 21:26:57 -0700

    Added in a config.rb which should listen on 0.0.0.0

---
+++ config.rb
@@ -0,0 +1,18 @@
+# Serve HTTP traffic on this port
+set  :port, 4567
+set  :host, 0.0.0.0
+
+riemann_base = '.'
+riemann_src = "#{riemann_base}/lib/riemann/dash"
+
+# Add custom controllers in controller/
+config.store[:controllers] = ["#{riemann_src}/controller"]
+
+# Use the local view directory instead of the default
+config.store[:views] = "#{riemann_src}/views"
+
+# Specify a custom path to your workspace config.json
+config.store[:ws_config] = "#{riemann_base}/config/config.json"
+
+# Serve static files from this directory
+config.store[:public] = "#{riemann_src}/public"
